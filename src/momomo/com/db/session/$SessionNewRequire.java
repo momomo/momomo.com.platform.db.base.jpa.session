24c0cdd388fad39d1a94f6d5747a773e04b30c06
momomo <opensource@momomo.com>
2021-03-22 16:31:26 +0100

    No message

--- src/momomo/com/db/session/$SessionNewRequire.java
+++ src/momomo/com/db/session/$SessionNewRequire.java
@@ -1,19 +1,17 @@
 package momomo.com.db.session;
 
-import momomo.com.annotations.informative.Private;
 import org.hibernate.Session;
 
 /**
  * @author Joseph S.
  */
 public interface $SessionNewRequire extends $SessionRepositoryDeclaration {
-    @Private boolean DEFAULT_OPEN = false;
     
     Session newSession    ();
     Session requireSession();
     
     default Session session() {
-        return session(DEFAULT_OPEN);
+        return session(false);
     }
     
     default Session session(boolean open) {
