cb5fab41bf7cdb7e84e68aa06ca2220bbfced650
momomo <opensource@momomo.com>
2021-03-23 11:26:59 +0100

    No message

--- src/momomo/com/db/$CriteriaLimit.java
+++ src/momomo/com/db/$CriteriaLimit.java
@@ -1,7 +1,6 @@
 package momomo.com.db;
 
 import org.hibernate.Criteria;
-import org.hibernate.HibernateException;
 import org.hibernate.criterion.CriteriaQuery;
 import org.hibernate.criterion.Criterion;
 import org.hibernate.engine.spi.TypedValue;
@@ -19,12 +18,12 @@ public class $CriteriaLimit implements Criterion {
     }
     
     @Override
-    public String toSqlString(Criteria criteria, CriteriaQuery criteriaQuery) throws HibernateException {
+    public String toSqlString(Criteria criteria, CriteriaQuery criteriaQuery) {
         criteria.setMaxResults(limit); return "1 = 1";
     }
     
     @Override
-    public TypedValue[] getTypedValues(Criteria criteria, CriteriaQuery criteriaQuery) throws HibernateException {
+    public TypedValue[] getTypedValues(Criteria criteria, CriteriaQuery criteriaQuery) {
         return new TypedValue[0];
     }
 }
