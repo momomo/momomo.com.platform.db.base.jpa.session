e1e7a13b165126c90dbb538c629eb5782cf9b475
momomo <opensource@momomo.com>
2021-03-25 09:11:10 +0100

    No message

--- src/momomo/com/db/sessionfactory/$SessionFactoryRepositoryHibernateDeclaration.java
+++ src/momomo/com/db/sessionfactory/$SessionFactoryRepositoryHibernateDeclaration.java
@@ -6,6 +6,4 @@ import momomo.com.db.session.$SessionRepositoryDeclaration;
  * @author Joseph S.
  */
 public interface $SessionFactoryRepositoryHibernateDeclaration extends $SessionRepositoryDeclaration {
-    @Override
-    $SessionFactoryRepositoryHibernate repository();
 }
