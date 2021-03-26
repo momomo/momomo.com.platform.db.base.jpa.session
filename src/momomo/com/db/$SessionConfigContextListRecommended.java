fdcb2983790e7c616f60921ae20eba10d69be047
momomo <opensource@momomo.com>
2021-03-26 21:29:42 +0100

    No message

--- src/momomo/com/db/$SessionConfigContextListRecommended.java
+++ src/momomo/com/db/$SessionConfigContextListRecommended.java
@@ -11,31 +11,27 @@ import java.util.Map;
 /**
  * Stores a 'stack' of active sessions rather than just one per thread. 
  *
- * Anytime get currentSession() is called, the last one to have registered is the one to use. 
- * This is to ensure that calls to openSession() which for our Transactional API means, start a new transaction, will also be registered.
+ * Anytime get currentSession() is called, the last one to have registered is the one to use by anyone 'requiring' a session. 
  * 
- * Currently, with Hibernate there is no way to reuse a session if you required a new one, which for newTransaction(...) would mean that a 
- * subsequent or nested call to requireTransaction would not recognize it, and hence return a new session itself.
+ * This also ensures that calls to openSession() which for our Transactional API means, start a new transaction, will also be registered.
+ * 
+ * Currently, with Hibernate there is no way to reuse a session if you required a new one, which for newTransaction(...) would mean that a subsequent or nested call to requireTransaction would not recognize it, and hence return a new session itself.
  * 
  * This is a flaw in Hibernate's **** API. 
- * It is a shame that major major parts of hibernates API and classes are a) so complex (unnecessarily) , b) declare private fields, methods and classes everywhere making extending them very complicated! 
+ * It is a shame that major major parts of Hiberante API and classes are a) so complex (unnecessarily) , b) declare private fields, methods and classes everywhere making extending them very complicated.  
  *
- * If you require advanced transaction capabilities, ie, nested transactions such as starting a new transaction while you have ongoing one 
- * this at all we suggest you use this one. 
+ * If you require advanced transaction capabilities, ie, nested transactions such as starting a new transaction while you have ongoing one, we suggest you use this one. 
+ * If not, you should be using the almost identical one {@link $SessionConfigContextSingleCrazyInsane} but which prevents Hibernate from wrapping the session instance normally done at times by Hibernate in {@link org.hibernate.context.internal.ThreadLocalSessionContext} to a wrapped and limited Session as {@link org.hibernate.context.internal.ThreadLocalSessionContext.TransactionProtectionWrapper}. 
+ * This is highly problematic, as you can not make certain method calls on the session which are perfectly valid to do, such as doWork(connection -> {}). 
  * 
- * If not, you should be using the almost identical one {@link $SessionConfigThreadLocalSessionContextUnwrappedTrackedSingleCrazyInsane} but which prevents Hibernate from wrapping the session instance normally done 
- * at times by Hibernate in {@link org.hibernate.context.internal.ThreadLocalSessionContext} to a wrapped and limited Session as {@link org.hibernate.context.internal.ThreadLocalSessionContext.TransactionProtectionWrapper}. 
- * 
- * Highly problematic, as you can not make certain method calls on the session which are perfectly valid to do, such as doWork(connection -> {}). 
- * 
- * We also provide {@link sessionfactory.$SessionFactory} implementation which also synchronize the main openSession() call as well which the default {@link org.hibernate.internal.SessionFactoryImpl} does not! 
+ * We also provide our own {@link momomo.com.db.sessionfactory.$SessionFactory} implementation that also synchronizes the main openSession() call as well which the default {@link org.hibernate.internal.SessionFactoryImpl} does not.  
  *
  * All our classes that implement our base class {@link $SessionConfigContextBase} such as
  * 
- *   - {@link $SessionConfigContextListRecommended}
- *   - {@link $SessionConfigThreadLocalSessionContextUnwrappedTrackedSingleCrazyInsane}  # Multiple sessions are not allowed. Based on Hibernates limited implementation which rollbacks and terminates any previous existing session on threadlocal while creating / opening a new one. Eats exception should there be one while rolling back and only logs it! Crazy, insane? They even roll you back! And if there is no error you won't know they rolled you! If there is an error, it is basically silent! No bubbling earning it the name Insane.  
- *   - {@link $SessionConfigThreadLocalSessionContextUnwrappedTrackedSingleCrazySane}    # Multiple sessions are not allowed. Based on Hibernates limited implementation which rollbacks and terminates any previous existing session on threadlocal while creating / opening a new one. Throws exception should there be one while rolling back. Crazy, sane? We still think rolling back is crazy, so we retain the crazy name, but at least is not is not silent, as we throw the rollback exception so it bubbles up should there be one, earning it the name Sane.  
- *   - {@link $SessionConfigThreadLocalSessionContextUnwrappedTrackedSingleCrazyLaxed}   # Multiple sessions are allowed    . Based on Hibernates limited implementation which does not terminate or do anything with previous session, allows it to coexist. No rollback occurs. The developer might still terminate, commit and manually handle any previous transaction created. Still Crazy, since when you close the new session, the previous one can not be accessed, despite the thread actually having one. Crazy, but Laxed.    
+ *   - {@link momomo.com.db.$SessionConfigContextListRecommended}
+ *   - {@link momomo.com.db.$SessionConfigContextSingleCrazyInsane}  # Multiple sessions are not allowed. Based on Hibernates limited implementation which rollbacks and terminates any previous existing session on threadlocal while creating / opening a new one. Eats exception should there be one while rolling back and only logs it Crazy, insane? They even roll you back! And if there is no error you won't know they rolled you! If there is an error, it is basically silent! No bubbling earning it the name Insane.  
+ *   - {@link momomo.com.db.$SessionConfigContextSingleCrazySane}    # Multiple sessions are not allowed. Based on Hibernates limited implementation which rollbacks and terminates any previous existing session on threadlocal while creating / opening a new one. Throws exception should there be one while rolling back. Crazy, sane? We still think rolling back is crazy, so we retain the crazy name, but at least is not is not silent, as we throw the rollback exception so it bubbles up should there be one, earning it the name Sane.  
+ *   - {@link momomo.com.db.$SessionConfigContextSingleCrazyLaxed}   # Multiple sessions are allowed    . Based on Hibernates limited implementation which does not terminate or do anything with previous session, allows it to coexist. No rollback occurs. The developer might still terminate, commit and manually handle any previous transaction created. Still Crazy, since when you close the new session, the previous one can not be accessed, despite the thread actually having one. Crazy, but Laxed.    
  *
  * What all of these does is to keep track of openSession() calls while Hibernate does not. A call to openSession() is required if you need a new Transaction, however a subsequent call to requireTransaction or getCurrentSession() won't get you the one started uing openSession() because it is untracked so you get TWO transactions while you likely expected it to coninue with the already existing one.   
  * 
@@ -46,19 +42,19 @@ import java.util.Map;
  * are still ABLE to keep track of openSession() calls in case you are not using our {@link sessionfactory.$SessionFactory} but only if you use our API to call openSession().
  * 
  * If you want to be sure you would need to tell Hiberante to use our {@link sessionfactory.$SessionFactory}. You can do this simply returning true in {@link $SessionConfig#useMomomoSessionFactory()}.   
- * but you still have to use an implementation based on {@link $SessionConfigContextBase}.
+ * but you still have to use an implementation based on {@link momomo.com.db.$SessionConfigContextBase}.
  * 
  * You would do this using
  * 
  *   properties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, {@link $SessionConfigContextListRecommended}.class.getName());
  * 
- * which is already default in our {@link $SessionConfig#properties()}
+ * which is already default in our {@link momomo.com.db.$SessionConfig#properties()}
  * 
  * You could also use one of the others
  * 
- *   properties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, {@link $SessionConfigThreadLocalSessionContextUnwrappedTrackedSingleCrazyInsane}.class.getName());
- *   properties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, {@link $SessionConfigThreadLocalSessionContextUnwrappedTrackedSingleCrazySane}.class.getName());
- *   properties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, {@link $SessionConfigThreadLocalSessionContextUnwrappedTrackedSingleCrazyLaxed}.class.getName());
+ *   properties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, {@link $SessionConfigContextSingleCrazyInsane}.class.getName());
+ *   properties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, {@link $SessionConfigContextSingleCrazySane}.class.getName());
+ *   properties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, {@link $SessionConfigContextSingleCrazyLaxed}.class.getName());
  *   
  * If you do not like any of this, and you do not care about registering openSession() calls, or nested or the ability to inherite any open transaction, you can still opt for Hibernate to track your threads as you've done in the past perhaps as you do not want to risk it (no risk really). 
  * We still however recommend you to use our version which simply does not wrap to a limited {@link org.hibernate.context.internal.ThreadLocalSessionContext.TransactionProtectionWrapper} which we can not work with in our transaction API if you want more features.
@@ -69,9 +65,9 @@ import java.util.Map;
  * This only tells the superclass {@link org.hibernate.context.internal.ThreadLocalSessionContext} not to wrap the session. Luckily needsWrapping method in superclass is not private, nor final! Thank you Hibernate for this terrible oversight! Next year they will hide it!
  * 
  * This version does not track openSession calls however. If you want the same but also want to register openSession() calls for nested or inheritable transactions regardless, you should be using
- *   properties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, {@link $SessionConfigThreadLocalSessionContextUnwrappedTrackedSingleCrazyInsane}.class.getName());
+ *   properties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, {@link $SessionConfigContextSingleCrazyInsane}.class.getName());
  *  
- *  Only difference between {@link $SessionConfigThreadLocalSessionContextUnwrappedUntrackedNotRecommended} and {@link $SessionConfigThreadLocalSessionContextUnwrappedTrackedSingleCrazyInsane} is that the latter will also track openSession calls. 
+ *  Only difference between {@link $SessionConfigThreadLocalSessionContextUnwrappedUntrackedNotRecommended} and {@link $SessionConfigContextSingleCrazyInsane} is that the latter will also track openSession calls. 
  *  
  * @author Joseph S.
  */
